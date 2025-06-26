faa228746fdfe9c1b9b23d02434948bbcc7b91fe
Drasko <drasko.miljkovic@gmail.com>
2025-06-26 09:33:41 +0200

    build optimization

--- docker/prod-entrypoint.sh
+++ docker/prod-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-# Migrate db
-pnpm -F backend migrate-production
+# Install dependencies first
+cd /usr/app
+pnpm install --prod
+
+# Run migrations using local knex
+cd packages/backend
+./node_modules/.bin/knex migrate:latest --knexfile dist/knexfile.js
 
 # Run prod
 exec "$@"
