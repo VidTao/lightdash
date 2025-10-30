bb7c175b0931f6e8669327da1bce6fc661991b48
Drasko995 <33683088+Drasko995@users.noreply.github.com>
2025-10-30 13:33:29 +0100

    Created file dbt/bratrax/models/lightdash/cod_test_2.sql

---
+++ dbt/bratrax/models/lightdash/cod_test_2.sql
@@ -0,0 +1,21 @@
+
+{{
+  config(
+    tags=['created-by-lightdash']
+  )
+}}
+  
+SELECT
+  `ads`.account_id AS `ads_account_id`,
+  `ads`.account_name AS `ads_account_name`,
+  `ads`.ad_id AS `ads_ad_id`
+FROM
+  `bratrax-without-flattening`.`production_tables`.`ads_view` AS `ads`
+GROUP BY
+  1,
+  2,
+  3
+ORDER BY
+  `ads_account_id`
+LIMIT
+  500
